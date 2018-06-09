f1072e0e01fa9a00d7e95c44cde2134f74017c38
ravencrest1985 <ravencrest1985@gmail.com>
2018-06-09 14:34:41 +0200

    Multi-Job (chunk and batchlet)

--- ChunkJob/src/main/java/chunk/CsvReader.java
+++ ChunkJob/src/main/java/chunk/CsvReader.java
@@ -1,6 +1,7 @@
 package chunk;
 
 import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.InputStreamReader;
 import java.io.Serializable;
 
@@ -10,10 +11,11 @@ import javax.inject.Named;
 @Named
 public class CsvReader extends AbstractItemReader {
 	BufferedReader rm;
-	public static final String FILE_NAME = "personData.csv";
+	public static final String FILE_NAME = "D:/end/personData.csv";
 	public void open(Serializable checkpoint) throws Exception {
 		rm = new BufferedReader(
-				new InputStreamReader(this.getClass().getClassLoader().getResourceAsStream(FILE_NAME), "UTF-8"));
+				//new InputStreamReader(this.getClass().getClassLoader().getResourceAsStream(FILE_NAME), "UTF-8"));
+				new FileReader(FILE_NAME));
 		rm.readLine();
 	}
 	@Override
